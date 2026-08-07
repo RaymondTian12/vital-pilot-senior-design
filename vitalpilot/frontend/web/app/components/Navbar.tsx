41fdc1e3211eda1ed6fedce308c3b7c327c5348d
pik8014 <pierre.konan@mavs.uta.edu>
2026-08-07 07:20:52 -0500

    Navbar implementation

--- vitalpilot/frontend/web/app/components/Navbar.tsx
+++ vitalpilot/frontend/web/app/components/Navbar.tsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import { PiSignInBold } from "react-icons/pi";
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div
+      className="bg-amber-200 fixed w-[80%] mx-auto mt-5 left-1/2 -translate-x-1/2 
+                  rounded-2xl h-17 flex justify-around items-center z-10"
+    >
+      <a href="" className="">
+        <Image
+          src="/assets/logo_hero.png"
+          alt="Vitalpilot"
+          width={160}
+          height={150}
+        />
+      </a>
+      <ul className="flex font-normal gap-6">
+        <li>
+          <a
+            className="bg-ai px-3 py-1 text-[16px] rounded-full font-bold 
+            font-quicksand flex-center gap-1 hover:text-black hover:bg-[#dce6fd]"
+            href=""
+          >
+            <Image
+              src="/assets/ai_black.png"
+              width={15}
+              height={15}
+              alt="ai-logo"
+            />
+            PILOT AI
+          </a>
+        </li>
+        <li>
+          <a className="font-normal" href="">
+            Doctor
+          </a>
+        </li>
+        <li>
+          <a className="font-normal" href="">
+            Dashboard
+          </a>
+        </li>
+        <li>
+          <a className="font-normal" href="">
+            Download App
+          </a>
+        </li>
+      </ul>
+      <div className="w-70 h-full flex-center gap-3">
+        <button>
+          <a className="flex-center gap-1" href="">
+            <PiSignInBold />
+            Sign in
+          </a>
+        </button>
+        <button>
+          <a
+            className="text-white px-5 py-2 bg-main rounded-[10px] hover:bg-secondary"
+            href=""
+          >
+            Get Started
+          </a>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
