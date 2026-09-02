2f293cbc5fa3feb141dd51aca95452afa678c444
pik8014 <pierre.konan@mavs.uta.edu>
2026-09-01 22:52:24 -0500

    implement dashboard sidebar

--- vitalpilot/frontend/web/app/components/Sidebar.tsx
+++ vitalpilot/frontend/web/app/components/Sidebar.tsx
@@ -1,9 +1,81 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+import { TbHeartPlus } from "react-icons/tb";
+import { TbReport } from "react-icons/tb";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { IoSettingsOutline } from "react-icons/io5";
+import { GoShieldCheck } from "react-icons/go";
+import { div } from "motion/react-client";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const pathname = usePathname();
 
-export default Sidebar
+  const links = [
+    {
+      name: "Dashboard",
+      href: "/dashboard",
+      icon: <MdOutlineDashboardCustomize />,
+    },
+    {
+      name: "Vitals",
+      href: "/dashboard/vitals",
+      icon: <TbHeartPlus />,
+    },
+    {
+      name: "Reports",
+      href: "/dashboard/reports",
+      icon: <TbReport />,
+    },
+    {
+      name: "Notifications",
+      href: "/dashboard/notifications",
+      icon: <IoNotificationsOutline />,
+    },
+    {
+      name: "Settings",
+      href: "/dashboard/settings",
+      icon: <IoSettingsOutline />,
+    },
+    {
+      name: "Security & Privacy",
+      href: "/dashboard/security",
+      icon: <GoShieldCheck />,
+    },
+  ];
+  return (
+    <div className="flex items-center flex-col w-60 border-r border-ai shadow-sm ">
+      <Link href="/" className="mt-2 mb-5">
+        <Image
+          src="/assets/logo_hero.png"
+          alt="Vitalpilot"
+          width={120}
+          height={120}
+          className="-translate-x-5"
+        />
+      </Link>
+      <div className="">
+        {links.map((link) => {
+          let active = pathname === link.href;
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className={`flex items-center gap-3 rounded-lg pl-3 py-3 mb-5 w-50 font-semibold ${active ? "bg-fourth/30 text-main" : "hover:bg-ai/50"}`}
+            >
+              {link.icon}
+              {link.name}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- vitalpilot/frontend/web/app/dashboard/page.tsx
+++ vitalpilot/frontend/web/app/dashboard/page.tsx
@@ -1,11 +1,83 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { MdLockOutline } from "react-icons/md";
 
 const Dashboard = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="flex justify-between flex-col h-full">
+      <div className="flex justify-between my-8 w-[90%] mx-auto">
+        <div>
+          <h3 className="font-bold mb-2">Welcome back, Username</h3>
+          <p className="font-medium text-gray-500">
+            Here's your health overview for today.
+          </p>
+        </div>
 
-export default Dashboard
+        <div className="flex items-start gap-10">
+          <IoNotificationsOutline className="mt-2 font-bold text-[24px]" />
+          <div className="flex items-center gap-3">
+            <div className="rounded-full w-10 h-10 bg-blue-50">
+              <Image src="" w-full h-full alt="" />
+            </div>
+            <div>first last</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col w-[90%] mx-auto gap-y-5 mb-10">
+        <div className="vitals"></div>
+        <div className="summary flex gap-x-5 ">
+          <div className="flex items-center justify-around basis-[65%] h-80 p-10 shadow-sm border border-ai rounded-xl bg-fourth/30">
+            <div className="flex flex-col w-[400] h-full">
+                <h4 className="font-bold text-lg flex items-center gap-2 mb-5">
+                <Image
+                  src="/assets/ai_green.png"
+                  width={240}
+                  height={240}
+                  alt=""
+                  className="w-5 h-5"
+                />{" "}
+                Pilot AI Insight
+              </h4>
+              <p className="font-semibold text-justify leading-relaxed indent-10">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus
+                doloribus saepe sunt officia deleniti sint. Assumenda, iure.
+                Architecto illo aut atque amet, ipsam sed reiciendis harum. Sit
+                architecto consequatur assumenda cupiditate cumque veritatis sed
+                perferendis unde officiis ullam! Labore obcaecati modi quae
+                nulla.
+              </p>
+            </div>
+            <div className="flex-center">
+              
+              <Image
+                src="/assets/chatbot-img.png"
+                width={1199}
+                height={1312}
+                alt=""
+                className="w-55 h-65"
+              />
+            </div>
+          </div>
+          <div className="basis-[35%] h-80 bg-white shadow-sm border border-ai rounded-xl"></div>
+        </div>
+        <div className="h-80 bg-white shadow-sm border border-ai rounded-xl px-10 py-5">
+          <div className="flex items-center justify-between">
+            <h4 className="font-bold text-lg">Recommended Doctors For You</h4>
+            <button className=" text-main font-bold">
+              view all
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <p className="flex-center gap-4 font-semibold text-[14px]">
+        <MdLockOutline /> Your date is private and secure
+      </p>
+    </div>
+  );
+};
+
+export default Dashboard;
