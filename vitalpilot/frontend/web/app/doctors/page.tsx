81ac06a286045773fb3743dbfe32975228772cdc
pik8014 <pierre.konan@mavs.uta.edu>
2026-09-17 21:04:58 -0500

    doctor ui done

--- vitalpilot/frontend/web/app/doctors/page.tsx
+++ vitalpilot/frontend/web/app/doctors/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
   return (
     <div className="">
       <Navbar />
-      <div className="relative max-w-[1500px] w-[90%] mx-auto h-[550px] rounded-3xl mt-5 drop-shadow-lg">
+      <div className="relative max-w-[1500px] w-[90%] mx-auto h-[550px] rounded-3xl drop-shadow-lg">
         <div
           className="w-full h-full absolute rounded-3xl top-0
       bg-[linear-gradient(90deg,rgba(255,255,255)_0%,rgba(255,255,255,.7)_40%,rgba(0,0,0,0)_80%)]"
@@ -50,31 +50,60 @@ const page = () => {
           </div>
         </div>
       </div>
-      <div className="flex bg-ai h-200 w-[80%] mx-auto mt-10 gap-3">
-        <aside className="basis-2/7 bg-yellow-100">
-            <div className="flex items-center justify-between border-1 border-gray">
-                <div className="">Filter <button>Reset</button></div>
-                <div>
-                    <div></div>
-                    <div>
-                        <input type="checkbox" name="" id="" />
-                        <input type="checkbox" name="" id="" />
-                        <input type="checkbox" name="" id="" />
-                        <input type="checkbox" name="" id="" />
-                        <input type="checkbox" name="" id="" />
-                        <input type="checkbox" name="" id="" />
-                        <input type="checkbox" name="" id="" />
-                        <input type="checkbox" name="" id="" />
-                    </div>
+      <div className="flex bg-ai/30 h-200 w-[80%] mx-auto mt-10 gap-5 p-5">
+        <aside className="flex flex-col basis-2/7 border border-gray bg-white p-4 font-medium rounded-lg gap-5">
+                <div className="flex justify-between mb-5">Filter <button className="cursor-pointer">Reset</button></div>
+                
+                <div className="font-semibold">
+                  <h4 className="mb-3">Provider gender</h4>
+                  <div className="flex gap-5">
+                    <button className="border border-gray rounded-full py-1 px-3 text-[14px]">Any</button>
+                    <button className="border border-gray rounded-full py-1 px-3 text-[14px]">Male</button>
+                    <button className="border border-gray rounded-full py-1 px-3 text-[14px]">Female</button>
+                  </div>
                 </div>
+                <div className="font-semibold">
+                    <h4 className="mb-3">Provider specialty</h4>
+                    <div className="flex gap-2 mb-3">
+                        <input type="checkbox" name="Bariatrician" id="specialty" className="w-5 accent-main"/>
+                        <label htmlFor="Bariatrician">Bariatrician</label>
+                    </div>
+                    <div className="flex gap-2 mb-3">
+                        <input type="checkbox" name="Cardiologist" id="specialty" className="w-5 accent-main"/>
+                        <label htmlFor="Cardiologist">Cardiologist</label>
+                    </div>
+                    <div className="flex gap-2 mb-3">
+                        <input type="checkbox" name="Endocrinologist" id="specialty" className="w-5 accent-main"/>
+                        <label htmlFor="Endocrinologist">Endocrinologist</label>
+                    </div>
+                    <div className="flex gap-2 mb-3">
+                        <input type="checkbox" name="Nephrologist" id="specialty" className="w-5 accent-main"/>
+                        <label htmlFor="Nephrologist">Nephrologist</label>
+                    </div>
+                    <div className="flex gap-2 mb-3">
+                        <input type="checkbox" name="Sleep" id="specialty" className="w-5 accent-main"/>
+                        <label htmlFor="Sleep">Sleep Medicine Physician</label>
+                    </div>
+                    <div className="flex gap-2 mb-3">
+                        <input type="checkbox" name="Sport" id="specialty" className="w-5 accent-main"/>
+                        <label htmlFor="Sport">Sport Medicine Physician</label>
+                    </div>
+                    <div className="flex gap-2 mb-3">
+                        <input type="checkbox" name="Pulmonologist" id="specialty" className="w-5 accent-main"/>
+                        <label htmlFor="Pulmonologist">Pulmonologist</label>
+                    </div>
             </div>
         </aside>
-        <div className="bg-amber-700 basis-5/7">
+        <div className="basis-5/7 flex flex-col ">
+          <h3 className="text-secondary font-bold mb-5">Best online doctors and providers, available now</h3>
+          <div className="self-end font-semibold">
             <label>Sort by</label>
-            <select name="" id="">
-                <option value="">Most relevant</option>
-                <option value="">Highest rated</option>
+            <select name="" id="" className="ml-3 bg-white py-2 px-3 drop-shadow-sm">
+                <option value="" className="">Most relevant</option>
+                <option value="" className="">Highest rated</option>
             </select>
+
+          </div>
         </div>
       </div>
     </div>
